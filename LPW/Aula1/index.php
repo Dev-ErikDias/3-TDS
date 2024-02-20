7e4e18016e48a80ed2f91f4adad7d56bd625af25
Dev-ErikDias <116355146+Dev-ErikDias@users.noreply.github.com>
2024-02-20 11:57:14 -0300

    Update index.php

--- LPW/Aula1/index.php
+++ LPW/Aula1/index.php
@@ -1,7 +1,27 @@
 <?php
 
-$a = 1;
-$b = 2;
-$soma = $a + $b;
+echo "Tabuada com WHILE";
+$num1 = 2;
+$num2 = 1;
+while($num2 <=   10){
+    $soma = $num1 * $num2;
+    echo "<br>$num1 X $num2 = $soma";
+    $num2 = $num2 + 1;
+}
 
-echo 'A soma é: '.$soma;
+echo "<br><br>Tabuada com DO-WHILE";
+$num2 = 1;
+do{
+    $soma = $num1 * $num2;
+    echo "<br>$num1 X $num2 = $soma";
+    $num2 = $num2 + 1;
+}while($num2 <= 10);
+
+
+echo "<br><br>Tabuada com FOR";
+
+for($i = 1; $i <=10;$i++){
+    $soma = $num1 * $i;
+    echo "<br>$num1 X $i = $soma";
+}
+
