c8d320e532a06be6cf96e08a7fa872ee70e29470
Dev-ErikDias <116355146+Dev-ErikDias@users.noreply.github.com>
2024-02-20 22:33:54 -0300

    Create Ex06.php

---
+++ LPW/Aula1/Ex06.php
@@ -0,0 +1,30 @@
+<?php 
+  for($i = 1; $i <= 8; $i++){
+    switch($i){
+      case 1:
+        echo '<div style="background-color: yellow; width: 100px; height: 100px;"></div>';
+        break;
+      case 2:
+        echo '<div style="background-color: blue; width: 100px; height: 100px;"></div>';
+        break;
+      case 3:
+        echo '<div style="background-color: black; width: 100px; height: 100px;"></div>';
+        break;
+      case 4:
+        echo '<div style="background-color: green; width: 100px; height: 100px;"></div>';
+        break;
+      case 5:
+        echo '<div style="background-color: red; width: 100px; height: 100px;"></div>';
+        break;
+      case 6:
+        echo '<div style="background-color: purple; width: 100px; height: 100px;"></div>';
+        break;
+      case 7:
+        echo '<div style="background-color: gray; width: 100px; height: 100px;"></div>';
+        break;
+      case 8:
+        echo '<div style="background-color: brown; width: 100px; height: 100px;"></div>';
+        break;
+    }
+  }
+?>
