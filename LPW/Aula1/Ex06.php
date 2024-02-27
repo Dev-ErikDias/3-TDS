c168cfdb16442075448f236c4e5cd1c99169f7ca
Dev-ErikDias <116355146+Dev-ErikDias@users.noreply.github.com>
2024-02-27 10:03:49 -0300

    Update Ex06.php

--- LPW/Aula1/Ex06.php
+++ LPW/Aula1/Ex06.php
@@ -1,30 +1,5 @@
 <?php 
+  $v = ["Yellow", "Blue", "Black", "Green", "Red", "Purple", "Gray", "Brown"];
   for($i = 1; $i <= 8; $i++){
-    switch($i){
-      case 1:
-        echo '<div style="background-color: yellow; width: 100px; height: 100px;"></div>';
-        break;
-      case 2:
-        echo '<div style="background-color: blue; width: 100px; height: 100px;"></div>';
-        break;
-      case 3:
-        echo '<div style="background-color: black; width: 100px; height: 100px;"></div>';
-        break;
-      case 4:
-        echo '<div style="background-color: green; width: 100px; height: 100px;"></div>';
-        break;
-      case 5:
-        echo '<div style="background-color: red; width: 100px; height: 100px;"></div>';
-        break;
-      case 6:
-        echo '<div style="background-color: purple; width: 100px; height: 100px;"></div>';
-        break;
-      case 7:
-        echo '<div style="background-color: gray; width: 100px; height: 100px;"></div>';
-        break;
-      case 8:
-        echo '<div style="background-color: brown; width: 100px; height: 100px;"></div>';
-        break;
-    }
+    echo "<div style='background-color: $v[$i]; width: 100px; height: 100px;'></div>";
   }
-?>
