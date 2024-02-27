7be6d07126a3522db0360bce1807962a753cbd82
Dev-ErikDias <116355146+Dev-ErikDias@users.noreply.github.com>
2024-02-27 11:42:43 -0300

    Update Ex01.php

--- LPW/Aula 2/Ex01.php
+++ LPW/Aula 2/Ex01.php
@@ -1,13 +1,12 @@
 <?php 
 
-  for($i = 5; $i <= 12; $i++){
+function fatorial($num){
+  $fatorial = 1;
+  for($i = $num; $i >= 1; $i--){
+    $fatorial *= $i; 
+  }
+  return $fatorial;
+}
+for($i = 5; $i <= 12; $i++){
     echo "<br>Fatorial de $i: ".fatorial($i);
-  }
-
-  function fatorial($num){
-    $fatorial = 1;
-    for($i = $num; $i >= 1; $i--){
-      $fatorial *= $i; 
-    }
-    return $fatorial;
-  }
+}
