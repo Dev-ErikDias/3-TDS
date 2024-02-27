69f9161475dfe688a7e10d6d56ff0f6c5849c6c6
Dev-ErikDias <116355146+Dev-ErikDias@users.noreply.github.com>
2024-02-27 11:47:41 -0300

    Create EX05.php

---
+++ LPW/Aula 2/EX05.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cidades</title>
+</head>
+<body>
+    <table>
+        <tr>
+            <td>
+                Numero 1
+            </td>
+            <td>
+                Nome 1
+            </td>
+        </tr>
+        <tr>
+            <td style="">
+                Numero 2
+            </td>
+            <td>
+                Nome 2
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
