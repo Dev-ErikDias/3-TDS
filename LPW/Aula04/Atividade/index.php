b7906b1eae6ae61b5c8b6c0b5bc326c1e0af9b33
Dev-ErikDias <116355146+Dev-ErikDias@users.noreply.github.com>
2024-03-11 09:04:57 -0300

    Create index.php

---
+++ LPW/Aula04/Atividade/index.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "<link rel="stylesheet" href="style.css">"
+
+function desenhaBotao($botao){
+    
+}
+
+$info1 = array("linkImg" => "", "info" => "");
+$info2 = array("linkImg" => "", "info" => "");
+$info3 = array("linkImg" => "", "info" => "");
+
+$times = array($info1, $info2, $info3);
+desenhaBotao($times);
