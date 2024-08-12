5fb81a5b1914d11f3ee0ace78c8eeb6e1fbbaae9
Dev-ErikDias <116355146+Dev-ErikDias@users.noreply.github.com>
2024-08-12 11:42:01 -0300

    Update criaClasses.php

--- Frameworks/Aula08/criaClasses.php
+++ Frameworks/Aula08/criaClasses.php
@@ -1,32 +1,33 @@
 <?php
-include "conexao.php";
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
-$query = $conn->query("show tables");
-$tabelas = $query->fetchAll(PDO::FETCH_OBJ);
-$conteudo = "";
+ include "conexao.php";
+ $query = $conn->query("show tables");
+ $tabelas = $query->fetchALL(PDO::FETCH_OBJ);
+ $conteudo="";
 
-foreach ($tabelas as $tabela) {
-    $conteudo .= "class ". ucfirst($tabela->Tables_in_framework). "{\n"; //ucfirst = Upper Case first (capitalize)
-    $queryAttr = $conn->query("show columns from ". $tabela->Tables_in_framework);
-    $atributos = $queryAttr->fetchAll(PDO::FETCH_OBJ);
-    foreach ($atributos as $atributo) {
-        $conteudo .= "private $". $atributo->Field. ";\n";
-    }
-
-    foreach ($atributos as $atributo) {
-        //Criando o set
-        $conteudo .= "public function set". ucfirst($atributo->Field). "($". $atributo->Field. "){
-            $".  "this->". ucfirst($atributo->Field). ";
-        }\n";
-
-        //Criando o get
-        $conteudo .= "public function get" . ucfirst($atributo->Field). "(){ 
-            return $". "this->". $atributo->Field. ";
-        }\n";
-    }
-
-    file_put_contents($tabela->tables_in_framework . ".php", $conteudo);
-    $conteudo = "";
-}
-echo $conteudo;
+ //var_dump($tabelas);
+ foreach($tabelas as $tabela){
+    $conteudo.="class ".ucfirst($tabela->Tables_in_framework)."{\n";
+    $queryAttr=$conn->query("show columns from ".$tabela->Tables_in_framework);
+    $atributos= $queryAttr->fetchAll(PDO::FETCH_OBJ);
+     
+    foreach($atributos as $atributo){
+	   $conteudo.="private $".$atributo->Field.";\n";
+	}
+     
+	foreach($atributos as $atributo){
+	   $conteudo.= "function get".ucfirst($atributo->Field)."(){
+	      return $"."this->".$atributo->Field.";}\n";
+	   $conteudo.="function set".ucfirst($atributo->Field)."($".$atributo->Field."){
+	     $"."this->".$atributo->Field."=$".$atributo->Field.";
+	 }\n";     
+	}
+	$conteudo .= "}";
+     
+	//echo $conteudo;
+    file_put_contents($tabela->Tables_in_framework.".php",$conteudo);
+    $conteudo="";
+ }
 ?>
