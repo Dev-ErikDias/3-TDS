002d25d05317a8d0413d99f984a8fa712bc90db8
Dev-ErikDias <116355146+Dev-ErikDias@users.noreply.github.com>
2024-09-16 11:53:49 -0300

    Update criarFormulario.php

--- Frameworks/Aula09/criarFormulario.php
+++ Frameworks/Aula09/criarFormulario.php
@@ -12,15 +12,28 @@ foreach ($tabelas as $tabela) {
 }
 echo "</select>";
 echo "<button type='submit'>Criar</button>";
+
+
 if ($_POST) {
     $queryAttr = $conn->query("show columns from " . $_POST["tabelas"]);
     $atributos = $queryAttr->fetchAll(PDO::FETCH_OBJ);
-    $conteudo = "<html>\n\t<head>\n\t<title>Cadastro</title>\n</head>\n\t<body>\n";
-    $conteudo .= "<form method 'post' action='#>\n";
-    foreach ($atributos as $atributo) {
-        $conteudo .= $atributo->Field . "<input type='text' name='$atributo->Field'><br>\n";
+    $conteudo = "<html>\n<head>\n\t<title>Cadastro</title>\n";
+    
+    $conteudo .= '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">';
+    $conteduo .= "\n". '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>';
+
+    $conteudo .= "\n</head>\n<body>\n";  //Tente fazer o bagl do floating
+    $conteudo .= "<form class='w-25 ' method='post' action='#'>\n";
+    $conteudo .= "<fieldset class='bg-black'>";
+    foreach ($atributos as $atributo){
+        if((strpos($atributo->Extra, 'auto_increment') === false) && (strpos($atributo->Extra, 'primary_key') === false)){
+            $conteudo .= "<div class='mb3'>\n";
+            $conteudo .= "\t<label class='text-light' for='$atributo->Field'>".ucfirst($atributo->Field).":</label> " . "\t<input class='bg.secondary-subtle' type='text' name='$atributo->Field'><br>\n";
+            $conteudo .= "\n<div>";
+        }
     }
-    $conteudo .= "<input type='submit' value='Cadastrar'>";
+    $conteudo .= "\n<input type='submit' value='Cadastrar'>";
+    $conteudo .= "\n</fieldset>";
     $conteudo .= "</form>\n";
     $conteudo .= "</body>\n</html>";
     file_put_contents("sistema/view/" . "Cadastro" . ucfirst($_POST['tabelas']) . ".php", $conteudo);
