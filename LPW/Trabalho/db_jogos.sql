3da17fae4d5cfb4e405f314a75ed39d47dddb38e
Dev-ErikDias <116355146+Dev-ErikDias@users.noreply.github.com>
2024-09-17 11:55:07 -0300

    Create db_jogos.sql

---
+++ LPW/Trabalho/db_jogos.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE db_jogos;
+USE db_jogos;
+CREATE TABLE jogos (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(50) NOT NULL,
+    genero VARCHAR(1) NOT NULL,
+    ano INTEGER NOT NULL,
+    tipo VARCHAR(3) NOT NULL,
+    preco INTEGER NOT NULL,
+    plataforma VARCHAR(5) NOT NULL,
+    faixa_etaria INTEGER NOT NULL,
+    CONSTRAINT pk_jogos PRIMARY KEY (id)
+);
