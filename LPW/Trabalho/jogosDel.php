5592d2c508b8ae613458d9e948cdc4cd359a2836
Dev-ErikDias <116355146+Dev-ErikDias@users.noreply.github.com>
2024-09-23 22:49:28 -0300

    Create jogosDel.php

---
+++ LPW/Trabalho/jogosDel.php
@@ -0,0 +1,26 @@
+<?php
+include_once('conexao.php');
+
+if(isset($_GET['id'])){
+    $id = $_GET['id'];
+    if($id){
+        $sql = "SELECT * FROM jogos WHERE id = ?";
+        $stm = $conn->prepare($sql);
+        $stm->execute([$id]);
+        $jogo = $stm->fetch();
+
+        if ($jogo) {
+            // Se o jogo existir, executa o DELETE
+            $sql = "DELETE FROM jogos WHERE id = ?";
+            $stm = $conn->prepare($sql);
+            $stm->execute([$id]);
+            header("Location: jogos.php");
+        } else {
+            // Se o jogo não existir, exibe mensagem de erro
+            echo "O ID informado não existe no banco de dados.";
+            echo '<a href="jogos.php">Voltar</a>';
+        }
+    }
+}
+
+?>
