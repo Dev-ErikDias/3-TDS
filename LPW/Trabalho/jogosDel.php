503ed71e701494010a954bb1b39744b7962699fb
Dev-ErikDias <116355146+Dev-ErikDias@users.noreply.github.com>
2024-09-24 09:39:13 -0300

    Update jogosDel.php

--- LPW/Trabalho/jogosDel.php
+++ LPW/Trabalho/jogosDel.php
@@ -3,24 +3,22 @@ include_once('conexao.php');
 
 if(isset($_GET['id'])){
     $id = $_GET['id'];
-    if($id){
-        $conn = Conexao::getConexao();
-        $sql = "SELECT * FROM jogos WHERE id = ?";
+    $conn = Conexao::getConexao();
+    $sql = "SELECT * FROM jogos WHERE id = ?";
+    $stm = $conn->prepare($sql);
+    $stm->execute([$id]);
+    $jogo = $stm->fetch();
+
+    if ($jogo) {
+        // Se o jogo existir, executa o DELETE
+        $sql = "DELETE FROM jogos WHERE id = ?";
         $stm = $conn->prepare($sql);
         $stm->execute([$id]);
-        $jogo = $stm->fetch();
-
-        if ($jogo) {
-            // Se o jogo existir, executa o DELETE
-            $sql = "DELETE FROM jogos WHERE id = ?";
-            $stm = $conn->prepare($sql);
-            $stm->execute([$id]);
-            header("Location: jogos.php");
-        } else {
-            // Se o jogo não existir, exibe mensagem de erro
-            echo "O ID informado não existe no banco de dados.";
-            echo '<a href="jogos.php">Voltar</a>';
-        }
+        header("Location: jogos.php");
+    } else {
+        // Se o jogo não existir, exibe mensagem de erro
+        echo "O ID informado não existe no banco de dados.";
+        echo '<a href="jogos.php">Voltar</a>';
     }
 }
 
