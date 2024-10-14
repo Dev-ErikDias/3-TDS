0b5233b7cd075a63b2d2ce281f60e9653c574b76
Dev-ErikDias <116355146+Dev-ErikDias@users.noreply.github.com>
2024-10-14 12:03:11 -0300

    Update pessoaControl.php

--- Frameworks/Aula11/control/pessoaControl.php
+++ Frameworks/Aula11/control/pessoaControl.php
@@ -1,1 +1,29 @@
+<?php
+require_once("../model/Pessoa.php");
+require_once("../dao/PessoaDao.php");
 
+class PessoaControl{
+    private $pessoa;
+    private $acao;
+    private $dao;
+
+    public function __construct(){
+        $this->pessoa=new Pessoas();
+        $this->dao = new PessoaDao();
+        $this->acao = $_GET['a'];
+        $this->verificaAcao();
+    }
+
+    function verificaAcao(){
+        switch($this->acao){
+            case 1:
+                $this->pessoa->setId($_POST['id']);
+                $this->pessoa->setNome($_POST['nome']);
+                $this->pessoa->setIdade($_POST['idade']);
+                $this->dao->alterar($this->pessoa);
+                break;
+        }
+    }
+}
+new PessoaControl();
+?>
