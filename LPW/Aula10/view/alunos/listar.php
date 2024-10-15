8b0258b94975266f21f73c10d52dafedc0c99a93
Dev-ErikDias <116355146+Dev-ErikDias@users.noreply.github.com>
2024-10-15 11:59:01 -0300

    Update listar.php

--- LPW/Aula10/view/alunos/listar.php
+++ LPW/Aula10/view/alunos/listar.php
@@ -1,48 +1,49 @@
-    <?php
+<?php
 
-    //Teste de conexão com a base de dados
-    /*
-    include_once(__DIR__.'/../../util/Connection.php');
-    $conn = Connection::getConnection();
-    print_r($conn);
-    */
+//Teste de conexão com a base de dados
+/*
+include_once(__DIR__.'/../../util/Connection.php');
+$conn = Connection::getConnection();
+print_r($conn);
+*/
 
-    include_once(__DIR__.'/../../controller/alunoController.php');
+include_once(__DIR__.'/../../controller/alunoController.php');
 
-    $alunoCont = new AlunoController();
-    $alunos = $alunoCont->listar();
+$alunoCont = new AlunoController();
+$alunos = $alunoCont->listar();
 
-    include_once(__DIR__.'/../include/header.php');
-    ?>
+include_once(__DIR__.'/../include/header.php');
+?>
 
-    <h2>Listagem de Alunos</h2>
+<h2>Listagem de Alunos</h2>
 
-    <table border="1">
-        <!-- Cabeçalho da tabela !-->
+<a href="inserir.php">Inserir aluno</a>
 
-        <thead>
-            <th>ID</th>
-            <th>Nome</th>
-            <th>Idade</th>
-            <th>Estrangeiro</th>
-            <th>Curso</th>
-        </thead>
+<table border="1">
+    <!-- Cabeçalho da tabela !-->
 
-        <!-- Dados da tabela !-->
-         <?php foreach($alunos as $a): ?>
+    <thead>
+        <th>ID</th>
+        <th>Nome</th>
+        <th>Idade</th>
+        <th>Estrangeiro</th>
+        <th>Curso</th>
+    </thead>
 
-            <tr>
-                <td><?= $a->getId();?></td>
-                <td><?= $a->getNome();?></td>
-                <td><?= $a->getIdade();?></td>
-                <td><?= $a->getEstrangeiroText();?></td>
-                <td><?= $a->getCurso();?></td>
-            </tr>
+    <!-- Dados da tabela !-->
+        <?php foreach($alunos as $a): ?>
 
-        <?php endforeach; ?>    
-    </table>
+        <tr>
+            <td><?= $a->getId();?></td>
+            <td><?= $a->getNome();?></td>
+            <td><?= $a->getIdade();?></td>
+            <td><?= $a->getEstrangeiroText();?></td>
+            <td><?= $a->getCurso();?></td>
+        </tr>
 
-    <?php
-    include_once(__DIR__.'/../include/footer.php');
+    <?php endforeach; ?>    
+</table>
 
-    ?>
+<?php
+include_once(__DIR__.'/../include/footer.php');
+?>
