345ef1064a407959ab502e90aade58878f6201cc
Dev-ErikDias <116355146+Dev-ErikDias@users.noreply.github.com>
2024-10-21 22:16:22 -0300

    Update inserir.php

--- LPW/Aula10/view/alunos/inserir.php
+++ LPW/Aula10/view/alunos/inserir.php
@@ -1,4 +1,12 @@
 <?php
+include_once(__DIR__.'/../../controller/alunoController.php');
 include("form.php");
+
+
+if($_POST){
+    $alunoCont = new AlunoController();
+    
+    echo $alunoCont->cadastrar($_POST);
+}
 
 ?>
