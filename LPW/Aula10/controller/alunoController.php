88223ac505235747cffc6fdc24c53839f9f467fc
Dev-ErikDias <116355146+Dev-ErikDias@users.noreply.github.com>
2024-10-21 22:14:07 -0300

    Update alunoController.php

--- LPW/Aula10/controller/alunoController.php
+++ LPW/Aula10/controller/alunoController.php
@@ -4,11 +4,17 @@ include_once(__DIR__.'/../dao/alunoDAO.php');
 class AlunoController{
 
     public function listar(){
-        $alunoDao = new alunoDAO();
+        $alunoDao = new AlunoDAO();
 
         $alunos = $alunoDao->list();
         return $alunos;
         
+    }
+
+    public function cadastrar(array $aluno){
+        $alunoDao = new AlunoDAO();
+
+        return $alunoDao->insert($aluno);
     }
 }
 
