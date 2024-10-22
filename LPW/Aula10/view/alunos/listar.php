b121fadc5053d6e96f778c8ae3e58d5efb7b7d93
Dev-ErikDias <116355146+Dev-ErikDias@users.noreply.github.com>
2024-10-21 22:16:44 -0300

    Update listar.php

--- LPW/Aula10/view/alunos/listar.php
+++ LPW/Aula10/view/alunos/listar.php
@@ -12,10 +12,44 @@ include_once(__DIR__.'/../../controller/alunoController.php');
 $alunoCont = new AlunoController();
 $alunos = $alunoCont->listar();
 
+for ($i = 0; $i < count($alunos); $i++) {
+    for ($j = $i + 1; $j < count($alunos); $j++) {
+        $orderBy = $_GET['list'] ?? 'id'; // Se $_GET['list'] não estiver definido, ordena por 'id'
+
+        switch ($orderBy) {
+            case 'course':
+                $ver = $alunos[$i]->getCurso() > $alunos[$j]->getCurso();
+                break;
+            case 'age':
+                $ver = $alunos[$i]->getIdade() > $alunos[$j]->getIdade();
+                break;
+            case 'id':
+            default:
+                $ver = $alunos[$i]->getId() > $alunos[$j]->getId();
+                break;
+        }
+
+        if($ver){
+            $aux = $alunos[$i];
+            $alunos[$i] = $alunos[$j];
+            $alunos[$j] = $aux;
+        }
+    }
+}
+
+
 include_once(__DIR__.'/../include/header.php');
 ?>
 
 <h2>Listagem de Alunos</h2>
+
+<div>
+    <form method="get" action="">
+        <button type="submit" name="list" value="id">Ordenar por ID</button>
+        <button type="submit" name="list" value="course">Ordenar por Curso</button>
+        <button type="submit" name="list" value="age">Ordenar por Idade</button>
+    </form>
+</div>
 
 <a href="inserir.php">Inserir aluno</a>
 
