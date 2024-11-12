421a5c9b826312aeee14bb407f97d2ea834260f5
Dev-ErikDias <116355146+Dev-ErikDias@users.noreply.github.com>
2024-11-12 11:49:36 -0300

    Update form.php

--- LPW/Aula10/view/alunos/form.php
+++ LPW/Aula10/view/alunos/form.php
@@ -1,54 +1,72 @@
 <?php
-include_once(__DIR__.'/../include/header.php');
-include_once(__DIR__.'/../../controller/cursoController.php');
+
+include_once(__DIR__."/../include/header.php");
+include_once(__DIR__."/../../controller/cursoController.php");
 
 $cursoCont = new CursoController();
 $cursos = $cursoCont->listar();
-
+//print_r($cursos);
 ?>
 
-<h3>Cadastrar Aluno</h3>
 
-<form id='formAluno' action="inserir.php" method="post">
+<form id="formAluno" method="post">
 
     <div>
-        <label for="nome">Nome:</label>
-        <input type="text" name="nome" id='txtNome' placeholder="Informe o nome" maxlength="70">
+        <label for="txtNome">Nome</label>
+        <input type="text" placeholder="Informe o nome" name="nome" id="textNome" maxlength="70" value="<?= $aluno != null ? $aluno->getNome() : ""?>">
     </div>
 
     <div>
-        <label for="idade">Idade:</label>
-        <input type="number" name="idade" id='txtIdade' placeholder="Informe a idade">
-    </div>  
+        <label for="txtIdade">idade</label>
+        <input type="number" placeholder="Informe a idade" name="idade" id="textIdade"  value="<?= $aluno != null ? $aluno->getIdade() : ""?>">
+    </div>
 
     <div>
-        <label for="selEtrang">Estrangeiro:</label>
-        <select name="estrang" id="selEstrang">
-            <option value="">--Selecione--</option>
-            <option value="T">Sim</option>
-            <option value="F">Não</option>
+        <label for="selEstrange">Estrangeiro</label>
+        <select name="estrang" id="selEstrange">
+        <option value="">------Selecione------ </option>
+        <option value="T" <?= $aluno != null && $aluno->getEstrangeiro() == 'T' ? 'selected':''?>>Sim</option>
+        <option value="F" <?= $aluno != null && $aluno->getEstrangeiro() == 'F' ? 'selected':''?>>Não</option>
         </select>
     </div>
 
     <div>
-        <label for="selCurso">Curso:</label>
-        <select name="curso" id="selCurso">
-            <option value="">--Selecione--</option>
-            <?php foreach($cursos as $c): ?>
-                <option value='<?=$c->getId() ?>'><?= $c?></option>
-            <?php endforeach; ?>  
+        <label for="selCurso">Selecione o Curso</label>
+        <select name="selCurso" id="selCurso">
+            <option value="">------Selecione------ </option>
+            <?php foreach($cursos as $curso):?>
+                <option value="<?= $curso->getId()?>"
+                <?php if($aluno != null && $aluno->getCurso()!= null && $aluno->getCurso()->getId() == $curso->getId()){
+                    echo "selected";
+                    }?>>
+                    <?= $curso?></option>
+            <?php endforeach?>   
+        
+       
         </select>
     </div>
+
+    <input type="hidden" name="id" value="<?= $aluno != null ? $aluno->getId() : ''?>">
 
     <div>
         <input type="submit" value="Gravar">
     </div>
+
+    
 </form>
 
-<div>
-    <a href="listar.php">Voltar</a>
+<div style='color: red;'>
+    <?=$msgErro?>
 </div>
 
+<div>
+    <a href="listar.php"><-----</a>
+</div>
+
+
+
+
+
 <?php
-include_once(__DIR__.'/../include/footer.php');
+include_once(__DIR__."/../include/footer.php");
 ?>
