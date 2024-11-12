abfb081ad559ccada79fe6e1612e4c90d0dd055a
Dev-ErikDias <116355146+Dev-ErikDias@users.noreply.github.com>
2024-11-12 11:51:25 -0300

    Update excluir.php

--- LPW/Aula10/view/alunos/excluir.php
+++ LPW/Aula10/view/alunos/excluir.php
@@ -1,13 +1,20 @@
 <?php
-include_once(__DIR__.'/../../controller/alunoController.php');
+#Página para excluir um aluno
 
+require_once(__DIR__ . "/../../controller/alunoController.php");
 
-if($_GET){
+//1- Capturar o ID do aluno utilizando a superglobal $_GET
+$id = isset($_GET['id']) && is_numeric($_GET['id']) ? trim($_GET['id']) : 0;
+
+if($id) {
+    //2- Chamar o AlunoController para excluir o aluno pelo ID 
+    //Excluir
     $alunoCont = new AlunoController();
+    $erros = $alunoCont->deletar($id);
     
-    $alunoCont->excluir($_GET['id']);
-
-    header("Location: listar.php");
+    //3- Voltar para o listar.php
+    header("location: listar.php");
+} else {
+    echo "Aluno não encontrado!<br>";
+    echo "<a href='listar.php'>Voltar</a>";
 }
-
-?>
