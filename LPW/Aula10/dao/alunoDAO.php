bd47c9887d35ee6ca32378365445055686e80190
Dev-ErikDias <116355146+Dev-ErikDias@users.noreply.github.com>
2024-11-12 11:51:47 -0300

    Update alunoDAO.php

--- LPW/Aula10/dao/alunoDAO.php
+++ LPW/Aula10/dao/alunoDAO.php
@@ -1,8 +1,9 @@
 <?php
-include_once(__DIR__.'/../util/Connection.php');
-include_once(__DIR__.'/../model/aluno.php');
+include_once(__DIR__."/../util/Connection.php");
+include_once(__DIR__."/../model/aluno.php");
+include_once(__DIR__."/../model/curso.php");
 
-class AlunoDAO{
+class AlunoDao{
 
     private $conn;
 
@@ -10,13 +11,28 @@ class AlunoDAO{
         $this->conn = Connection::getConnection();
     }
 
+    public function delete($id){
+       $sql = "DELETE FROM alunos WHERE id=?";
+       $stm=$this->conn->prepare($sql);
+       $stm->execute(array($id));
+    }
+
+    public function insert(Aluno $alunos){
+        $sql = "INSERT INTO alunos(nome, idade, estrangeiro, id_curso)
+                VALUES (?,?,?,?)";
+        $stm=$this->conn->prepare($sql);
+        $stm->execute(array($alunos->getNome(),
+                            $alunos->getIdade(),
+                            $alunos->getEstrangeiro(),
+                            $alunos->getCurso()->getId()));
+    }
+
     public function list(){
         $sql = "SELECT a.*, c.nome curso_nome, c.turno curso_turno 
         FROM alunos a 
-        JOIN cursos c 
-        ON (c.id = a.id_curso);";
+        JOIN cursos c ON (c.id= a.id_curso)";
 
-        $stm = $this->conn->prepare($sql);
+        $stm =$this->conn->prepare($sql);
         $stm->execute();
         $result = $stm->fetchAll();
 
@@ -24,32 +40,56 @@ class AlunoDAO{
         return $alunos;
     }
 
-    public function insert(Aluno $aluno){
-        $sql = "INSERT INTO alunos (nome, idade, estrangeiro, id_curso) VALUES (?, ?, ?, ?)";
+    public function findById(int $id) {
+        $sql = "SELECT a.*, c.nome curso_nome, c.turno curso_turno
+                FROM alunos a
+                JOIN cursos c ON (c.id = a.id_curso)
+                WHERE a.id = ?";
 
-        $stm = $this->conn->prepare($sql);
-        $stm->execute(array($aluno->getNome(), $aluno->getIdade(), $aluno->getEstrangeiro(), $aluno->getCurso()->getId()));
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute([$id]);
+        $result = $stmt->fetchAll();
+
+        //Criar o objeto Aluno
+        $alunos = $this->mapAlunos($result);
+
+        if(count($alunos) == 1)
+            return $alunos[0];
+        elseif(count($alunos) == 0)
+            return null;
+
+        die("AlunoDAO.findById - Erro: mais de um aluno".
+                " encontrado para o ID " . $id);
     }
 
-    public function delete($id){
-        $sql = "DELETE FROM alunos WHERE id=?";
-        $stm = $this->conn->prepare($sql);
-        $stm->execute(array($id));
-    }
 
+    public function update(Aluno $aluno) {
+        $conn = Connection::getConnection();
+
+        $sql = "UPDATE alunos SET nome = ?, idade = ?,". 
+            " estrangeiro = ?, id_curso = ?".
+            " WHERE id = ?";
+        $stmt = $conn->prepare($sql);
+        
+        $stmt->execute([$aluno->getNome(), $aluno->getIdade(), 
+                        $aluno->getEstrangeiro(), $aluno->getCurso()->getId(),
+                        $aluno->getId()]);
+    }
+   
     private function mapAlunos(array $result){
+
         $alunos = array();
 
-        foreach ($result as $reg) {
+        foreach($result as $reg){
             $aluno = new Aluno();
-            $aluno->setId($reg['id']);
-            $aluno->setNome($reg['nome']);
-            $aluno->setEstrangeiro($reg['estrangeiro']);
-            $aluno->setIdade($reg['idade']);
+            $aluno->setId($reg["id"]);
+            $aluno->setIdade($reg["idade"]);
+            $aluno->setNome($reg["nome"]);
+            $aluno->setEstrangeiro($reg["estrangeiro"]);
 
             $curso = new Curso();
-            $curso->setID($reg['id_curso']);
-            $curso->setNome($reg['curso_nome']);
+            $curso->setId($reg['id_curso']);
+            $curso->setNome(($reg['curso_nome']));
             $curso->setTurno($reg['curso_turno']);
             $aluno->setCurso($curso);
 
@@ -58,7 +98,5 @@ class AlunoDAO{
 
         return $alunos;
     }
-
 }
-
 ?>
