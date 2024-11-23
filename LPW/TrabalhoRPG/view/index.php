2168081cd5188d0dcd317601e51ded84026eb377
Dev-ErikDias <116355146+Dev-ErikDias@users.noreply.github.com>
2024-11-23 17:09:11 -0300

    Create index.php

---
+++ LPW/TrabalhoRPG/view/index.php
@@ -0,0 +1,14 @@
+<?php
+include_once(__DIR__."/include/header.php");
+include_once(__DIR__."/../controller/personagemController.php");
+$personagemCont = new PersonagemController();
+
+?>
+
+<div class="container mt-4">
+    <div class="row">
+
+<?php
+    include_once(__DIR__."/form.php");
+    include_once(__DIR__."/include/footer.php");
+?>
