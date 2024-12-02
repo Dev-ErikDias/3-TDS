7a5776282117e054010c6b3c0aca9c282a372c13
Dev-ErikDias <116355146+Dev-ErikDias@users.noreply.github.com>
2024-12-02 11:33:48 -0300

    Create conexao.php

---
+++ Frameworks/Aula13/conexao.php
@@ -0,0 +1,17 @@
+<?php
+
+class Conexao{
+
+    public static function conectar(){
+        try{
+            $conn = new PDO("mysql:host=localhost;dbname=endereco", "root", "bancodedados");
+        }
+        catch(Exception $e){
+            echo "Erro :". $e->getMessage();
+        }
+        return $conn;
+    }
+
+}
+
+?>
